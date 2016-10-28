f805be8618359b2206b649fe056df776c3ba0e53
huangpeng <huangpeng@chunyu.me>
2016-10-28 16:27:31 +0800

    init

---
+++ app/src/main/java/com/beacon/searchdemo/MainActivity.java
@@ -0,0 +1,94 @@
+package com.beacon.searchdemo;
+
+import android.app.SearchManager;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String DATA_PATH = Environment
+            .getExternalStorageDirectory().toString() + "/SimpleAndroidOCR/";
+
+    private static final String TAG = "MainActivity.java";
+
+    private String mPath;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SearchActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mPath = DATA_PATH + "/ocr.jpg";
+
+        FloatingActionButton ocr = (FloatingActionButton)findViewById(R.id.fab_ocr);
+        ocr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                File file = new File(mPath);
+                Uri outputFileUri = Uri.fromFile(file);
+
+                final Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        Log.i(TAG, "resultCode: " + resultCode);
+
+        if (resultCode == -1) {
+            OCR.regcognize(mPath);
+        } else {
+            Log.v(TAG, "User cancelled");
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/beacon/searchdemo/OCR.java
@@ -0,0 +1,16 @@
+package com.beacon.searchdemo;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by huangpeng on 10/28/16.
+ */
+public class OCR {
+    public static String regcognize(String path) {
+        return "recognize path";
+    }
+
+    public static String recognize(Bitmap image) {
+        return "recognize image";
+    }
+}
